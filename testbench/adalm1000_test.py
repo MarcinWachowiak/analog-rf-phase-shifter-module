df57fd48dd4e2f8f81e877561711e8c4734be784
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2022-02-20 20:59:51 +0100

    Added some test plots

--- testbench/adalm1000_test.py
+++ testbench/adalm1000_test.py
@@ -44,15 +44,15 @@
 #
 # Simple script showing how to read and write data to a device in continuous
 # mode, use Ctrl-C to exit.
-
+#%%
 from __future__ import print_function
-
 from signal import signal, SIG_DFL, SIGINT
 import random
 import sys
 import time
-
+import numpy as np
 from pysmu import Session, Mode
+import matplotlib.pyplot as plt
 
 
 # If stdout is a terminal continuously overwrite a single line, otherwise
@@ -71,7 +71,7 @@ def refill_data(num_samples, v=None):
     # fill channels with a static, random float between -0.2 and 0.2
     #return [random.uniform(-0.2,0.2)] * num_samples
 
-
+#%%
 if __name__ == '__main__':
     # don't throw KeyboardInterrupt on Ctrl-C
     signal(SIGINT, SIG_DFL)
@@ -89,29 +89,53 @@ if __name__ == '__main__':
         chan_a = dev.channels['A']
         chan_b = dev.channels['B']
         chan_a.mode = Mode.SVMI
-        chan_b.mode = Mode.SVMI
+        chan_b.mode = Mode.HI_Z
         #chan_a.mode = Mode.SIMV
         #chan_b.mode = Mode.SIMV
 
         # Start a continuous session.
         session.start(0)
-        i = 0
+        idx = 0
+        vbias_vec = np.arange(0.0, 5.1, 0.05)
+        print(vbias_vec)
+        v_out = np.zeros(len(vbias_vec))
+
         num_samples = session.queue_size + 1
         start = time.time()
 
+        tmp_arr = []
         while True:
+
             # Change written value approximately every second.
-            if time.time() - start > 0.5:
-                i += 1
+            if time.time() - start > 0.1:
+                
+                v_out[idx] = np.average(np.array(tmp_arr))
+                tmp_arr = []
+                idx += 1
+                # print("ITE: ", idx)
                 start = time.time()
 
+            if idx > (len(vbias_vec) - 1):
+                break
+
             # Write iterating voltage values to both channels.
-            chan_a.write(refill_data(num_samples, 0.25 * (i % 21)), -1)
-            chan_b.write(refill_data(num_samples, 0.25 * (i % 21)), -1)
+            chan_a.write(refill_data(num_samples, vbias_vec[idx]), -1)
 
             # Read incoming samples in a non-blocking fashion.
             samples = dev.read(num_samples)
+            
             for x in samples:
-                output("{: 6f} {: 6f} {: 6f} {: 6f}".format(x[0][0], x[0][1], x[1][0], x[1][1]))
+                tmp_arr.append(x[1][0])
+
+
     else:
         print('no devices attached')
+
+    #%%
+    print("Plot data!")
+    print(v_out)
+    plt.plot(vbias_vec, v_out)
+    plt.xlabel("Vbias [V]")
+    plt.ylabel("AD8302 Vout [V]")
+    plt.savefig("freq_2_45GHz_ad8302_output")
+    plt.show()
